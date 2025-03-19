34e07586596d9e08b14720a3ea2233441c3ab17a
DimsEps <dimaskld4@gmail.com>
2025-03-19 11:14:40 +0700

    dimas first commit

---
+++ Kuis 1/DimasEkaPutraSantoso/index.js
@@ -0,0 +1,15 @@
+const nama = "Dimas Eka Putra Santoso";
+const jurusan = "Teknik Informatika";
+const npm = "2315061114";
+
+// Menampilkan di Console
+function showConsole() {
+  console.log("Nama    : " + nama);
+  console.log("Jurusan : " + jurusan);
+  console.log("NPM     : " + npm);
+}
+
+// Menampilkan di Pop-Up
+function showAlert() {
+  alert(`Nama: ${nama}\nJurusan: ${jurusan}\nNPM: ${npm}`);
+}
