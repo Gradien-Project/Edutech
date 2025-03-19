20b5328b48ae63e3d5413112874bb3bbb3fc8951
kite1412 <frozradit@gmail.com>
2025-03-19 11:43:44 +0700

    natha

---
+++ Kuis 1/NathaRadityaRauf/info.js
@@ -0,0 +1,9 @@
+const nama = "Natha Raditya Rauf";
+const jurusan = "Teknik Informatika";
+const npm = "2315061062";
+
+console.log("Nama    : " + nama);
+console.log("Jurusan : " + jurusan);
+console.log("NPM     : " + npm);
+
+alert(`Nama: ${nama}\nJurusan: ${jurusan}\nNPM: ${npm}`);
